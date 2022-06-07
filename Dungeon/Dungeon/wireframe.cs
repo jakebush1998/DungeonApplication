b3de7769a3f3b9e06f805ca43b49f00a67ccdb9d
jakebush1998 <Student@LAPTOP-CB78AN7D>
2022-06-07 08:24:26 -0500

    Made class and began to code out wireframe

---
+++ Dungeon/Dungeon/wireframe.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dungeon
+{
+    internal class wireframe
+    {
+        static void Main(string[] args)
+        {
+            //TODO Create A Player
+            Console.WriteLine("Dungeon of Death");
+            bool play = true; //COUNTER
+
+            do
+            {
+                Console.WriteLine("A. Attack\nB.Run Away\nC. Character Info\nD. Monster Info\nE. Exit");
+                string playerChoice = Console.ReadLine().ToUpper();
+
+                Console.Clear();
+
+                switch (playerChoice)
+                {
+                    case "A":
+                    case "Attack":
+                        Console.WriteLine();
+                        break;
+
+                    case "B":
+                    case "RUN AWAY":
+                        Console.WriteLine();
+                        break;
+
+                    case "C":
+                    case "CHARACTER INFO":
+                        Console.WriteLine();
+                        break;
+
+                    case "D":
+                    case "MONSTER INFO":
+                        Console.WriteLine();
+                        break;
+
+                    case "E":
+                    case "EXIT":
+                        Console.WriteLine();
+                        play = false;
+                        break;
+                }
+
+
+            } while (play); //CONDITION
+
+
+
+        }
+    }
+}
